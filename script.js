507cc0f6feb0a98fc299d033f3d7c97d03cb773c
GaryVH01 <gary.vanhecke@gmail.com>
2025-01-10 16:53:06 +0100

    workin responsive

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const navMobile = document.querySelector(".nav-mobile");
 const closeIcon = document.querySelector(".close-icon");
 const menuIcon = document.querySelector(".burger-icon");
+const headerIcons = document.querySelector(".header-icons");
+const searchIcon = document.querySelector(".header-icons-search input");
 
+// Forcer la fermeture du menu burger au chargement de la page
+document.addEventListener("DOMContentLoaded", function () {
+  navMobile.style.display = "none";
+});
+
+// Fonction permettant d'ouvrir et fermer le menu burger
 menuIcon.addEventListener("click", () => {
   navMobile.style.display = "flex";
+  // headerIcons.style.display = "none";
 });
 
 closeIcon.addEventListener("click", () => {
   navMobile.style.display = "none";
+  // headerIcons.style.display = "flex";
 });
+
+// searchIcon.addEventListener("click", () => {
+//   searchIcon.style.display = "flex";
+// });
